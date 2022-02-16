8acf07d759da3e9fe830bd5fc3aac2e0306cfaad
ischenysh <chen.ysh@outlook.com>
2022-02-16 12:36:21 +0800

    log输出重定向

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"bytedance/router"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
 )
 
 func main() {
@@ -13,5 +15,12 @@ func main() {
 	db.InitDB()              // 数据库
 	redis_server.InitRedis() // redis连接
 	router.RegisterRouter(r) // 路由
+	// 日志输出到文件
+	file := "./" + "message" + ".log"
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(logFile) // 将文件设置为log输出的文件
 	r.Run(":80")
 }
